12c52fbcfeaa28da46be28e8a56610477a53d0d0
andre ferrer <andrerferrer@hotmail.com>
2021-05-13 04:37:56 -0300

    Quicksave

---
+++ src/components/get_movies_api.js
@@ -0,0 +1,45 @@
+const insertIntoTheDOM = (movie) => {
+  const movieHTML = `
+    <li class="list-group-item">
+      <p>${movie.Title}</p>
+      <img style='height: 240px;' src='${movie.Poster}'>
+    </li>
+  `
+  const results = document.getElementById('results')
+  results.insertAdjacentHTML('beforeend', movieHTML);  
+};
+
+const fetchAPI = (movieName) => {
+  const url = `http://www.omdbapi.com/?apikey=adf1f2d7&s=${movieName}`;
+  // AJAX
+  // Async Javascript
+  
+  // fetch the url of the api
+  fetch(url)
+    // wait for the response and parse it into json
+    .then(response => response.json())
+    // then, call this json, call it data and do something
+    .then((data) => {
+      console.log(data);
+  
+      const movies = data.Search; // dig the json to find what we want
+      movies.forEach(movie => insertIntoTheDOM(movie));
+    });
+};
+
+const getInputAndCallAPI = (event) => {
+  // by default, the submit event 'reloads the page'
+  event.preventDefault();
+  
+  // take the user input
+  const userInput = document.getElementById('keyword').value;
+
+  // empty the content of the ul before fetching the api
+  const results = document.getElementById('results');
+  results.innerHTML = '';
+
+  // fetch the api with that input
+  fetchAPI(userInput);
+};
+
+export { fetchAPI, getInputAndCallAPI };
--- src/index.js
+++ src/index.js
@@ -1,54 +1,12 @@
-// FUNCTIONS //
-///////////////
+import { fetchAPI, getInputAndCallAPI } from './components/get_movies_api';
 
-const insertIntoTheDOM = (movie) => {
-  const movieHTML = `
-    <li>
-      <p>${movie.Title}</p>
-      <img src='${movie.Poster}'>
-    </li>
-  `
-  document.getElementById('results')
-          .insertAdjacentHTML('beforeend', movieHTML);  
-}
+/////////////////////////////////////////
+// EVENT LISTENERS, QUERY SELECTORS AND FUNCTION CALLS //
+/////////////////////////////////////////
 
-
-/////////////////////
-// THE APP RUNNING //
-/////////////////////
-
-const fetchAPI = (movieName) => {
-  const url = `http://www.omdbapi.com/?apikey=adf1f2d7&s=${movieName}`;
-  // AJAX
-  // Async Javascript
-  
-  // fetch the url of the api
-  fetch(url)
-    // wait for the response and parse it into json
-    .then(response => response.json())
-    // then, call this json, call it data and do something
-    .then((data) => {
-      console.log(data);
-  
-      const movies = data.Search;
-      movies.forEach(movie => insertIntoTheDOM(movie));
-    });
-}
+fetchAPI('superman');
 
 // form
 const form = document.querySelector('form#search-movies');
 // when the user submits the form
-form.addEventListener('submit', (event) => {
-  // by default, the submit event 'reloads the page'
-  event.preventDefault();
-  
-  // take the user input
-  const userInput = document.getElementById('keyword').value;
-
-  // empty the content of the ul before fetching the api
-  const results = document.getElementById('results')
-  results.innerHTML = ''
-
-  // fetch the api with that input
-  fetchAPI(userInput);
-})
+form.addEventListener('submit', getInputAndCallAPI);
